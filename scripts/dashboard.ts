792419adae51d5f6b6570fe6775a3ea4bec85b0f
Giovax0Fail <g.ferrante5@outlook.com>
2024-07-01 12:10:25 +0200

    Add rest requests and dashboard

---
+++ scripts/dashboard.ts
@@ -0,0 +1,15 @@
+async function getData() {
+    try {
+        const response = await fetch("https://example.org/products.json", {
+            headers: { Authorization: "Bearer " + token },
+        });
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+    } catch (error: any) {
+        console.error(error.message);
+    }
+}
