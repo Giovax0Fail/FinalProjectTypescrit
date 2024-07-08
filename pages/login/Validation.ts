b54cb111b959b71d167a7e40617d3902d19087d7
Giovax0Fail <g.ferrante5@outlook.com>
2024-07-08 21:40:15 +0200

    "Regex validation ts"

---
+++ pages/login/Validation.ts
@@ -0,0 +1,17 @@
+function validate(): void {
+    const emailElement = document.getElementById('email') as HTMLInputElement | null;
+    
+    if (emailElement) {
+      const email: string = emailElement.value;
+      
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if(regex.test(email)) {
+        alert("Mail Valida.");
+      } else {
+        alert("Mail non valida! Ricontrolla");
+      }
+    } else {
+      alert("Email non immessa");
+    }
+  }
+  
