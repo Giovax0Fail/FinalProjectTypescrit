51299e14581ea7f0e208e7fce6894b861d3f1f41
Giovax0Fail <g.ferrante5@outlook.com>
2024-07-12 10:03:36 +0200

    removed email validation because it activates every time

--- pages/login/Validation.js
+++ pages/login/Validation.js
@@ -1,17 +1,15 @@
 "use strict";
-function validate() {
-    const emailElement = document.getElementById('email');
-    if (emailElement) {
-        const email = emailElement.value;
-        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (regex.test(email)) {
-            alert("Mail Valida.");
-        }
-        else {
-            alert("Mail non valida! Ricontrolla");
-        }
-    }
-    else {
-        alert("Email non immessa");
-    }
-}
+// function validate(): void {
+//     const emailElement = document.getElementById('email') as HTMLInputElement | null;
+//     if (emailElement) {
+//       const email: string = emailElement.value;
+//       const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//       if(regex.test(email)) {
+//         alert("Mail Valida.");
+//       } else {
+//         alert("Mail non valida! Ricontrolla");
+//       }
+//     } else {
+//       alert("Email non immessa");
+//     }
+//   }
